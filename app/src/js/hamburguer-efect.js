23e0716b011f7691c0030b0ffb07b902f9a73585
ruan ramalho <ruanmbramalho@gmail.com>
2020-06-08 14:36:28 -0300

    fix(header): correcting header nav

--- app/src/js/hamburguer-efect.js
+++ app/src/js/hamburguer-efect.js
@@ -3,5 +3,6 @@ $(function() {
   $(".menu").on("click", function() {
     $(this).find(".hamburguer").toggleClass("active");
     $(".menu").find(".navbar").toggleClass("hide");
+    console.log("pegou")
   });
 });
