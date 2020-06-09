5299d1037d69808dfb60f8e09afde50f3b06fdb7
ruan ramalho <ruanmbramalho@gmail.com>
2020-06-09 11:20:05 -0300

    refactor(header): removing console javascript

--- app/src/js/hamburguer-efect.js
+++ app/src/js/hamburguer-efect.js
@@ -1,5 +1,4 @@
 $(function() {
-  console.log( "ready!" );
   $(".menu").on("click", function() {
     $(this).find(".hamburguer").toggleClass("active");
     $(".menu").find(".navbar").toggleClass("hide");
